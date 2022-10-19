6d1f8c5e02a2e85e2957a5db1e1201b9689a95c9
xlr8s <xlr8s@xebia.com>
2022-10-19 09:41:03 +0000

    added terraform resources

---
+++ envs/dev/01-infra/eks-dev-local-first-6123/03-eks/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {	
+    bucket = "xlr8s-artifacts"
+    encrypt = "false"
+    key = "local-first-6123/envs/dev/01-infra/eks-dev-local-first-6123/03-eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
